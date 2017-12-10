e66ab9ed515b41ff6f906a402d35311577e20ff8
Santiago Saucedo <santiago.g.saucedo@gmail.com>
2017-12-10 16:21:33 -0300

    (feat) New container for flight edition

--- app/index.js
+++ app/index.js
@@ -1,16 +1,10 @@
 import 'babel-polyfill'
 /* Support for IE11 */
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Provider, connect } from 'react-redux'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import store from './generics/store'
-import { OPERATIONS } from './services/flights/sagas'
-import { or, equals } from 'ramda'
-import { Flights } from './components/Flights'
-import { FlightEdition } from './components/FlightEdition'
-import {getOperationInfo, triggerAction} from './containers/helpers'
-import { FlowTreeGraph } from './graph/tree'
+import FlightEditionContainer from './containers/FlightEditionContainer'
 
 /*
  * If given more time, this should be separated into different
@@ -18,70 +12,21 @@ import { FlowTreeGraph } from './graph/tree'
  * */
 class App extends React.PureComponent {
 	render () {
-		const {dispatch} = this.props
-		const {BOOK_FLIGHT, FLIGHT_EDITION} = OPERATIONS
-		const flightComponent = <Flights
-			BOOK_FLIGHT={BOOK_FLIGHT}
-			{...this.props.bookFlight}
-			triggerAction={triggerAction(dispatch)}/>
-
-		const flightEdition = <FlightEdition
-			FLIGHT_EDITION={FLIGHT_EDITION}
-			{...this.props.flightEdition}
-			triggerAction={triggerAction(dispatch)}/>
-
-		//{flightComponent}
 		return (
 			<div>
-				{flightEdition}
-				<FlowTreeGraph step={or(this.props.bookFlight.step, '')} steps={BOOK_FLIGHT.steps}/>
+				<FlightEditionContainer/>
 			</div>
 		)
 	}
 }
 
-const mapStateToProps = ({operations}) => {
-	const bookFlightOperation = or(or(operations.inProgress, {})[OPERATIONS.BOOK_FLIGHT.name], {})
-	return {
-		bookFlight: {
-			...getOperationInfo(OPERATIONS.BOOK_FLIGHT.name, operations),
-			locations: equals(bookFlightOperation.step, OPERATIONS.BOOK_FLIGHT.steps.SELECT_ORIGIN) ? bookFlightOperation.state.locations : [],
-		},
-		flightEdition: {
-			...getOperationInfo(OPERATIONS.FLIGHT_EDITION.name, operations),
-		},
-	}
-}
-
-const AppWithState = connect(mapStateToProps)(App)
-
 const render = () => {
 	ReactDOM.render(
 		<Provider store={store}>
-			<AppWithState />
+			<App />
 		</Provider>,
 		document.getElementById('app')
 	)
-}
-
-App.propTypes = {
-	dispatch: PropTypes.func.isRequired,
-	bookFlight: PropTypes.shape({
-		operation: PropTypes.object,
-		locations: PropTypes.any,
-		succeed: PropTypes.object,
-		failed: PropTypes.object,
-		step: PropTypes.string,
-		steps: PropTypes.string,
-	}),
-	flightEdition: PropTypes.shape({
-		operation: PropTypes.object,
-		succeed: PropTypes.object,
-		failed: PropTypes.object,
-		step: PropTypes.string,
-		steps: PropTypes.string,
-	}),
-
 }
 
 /* prevent FOUC https://stackoverflow.com/a/43902734 */
